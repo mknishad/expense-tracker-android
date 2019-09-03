81f2b966b563d01ee437b1b02bb463194da40037
Monir Hossain <mknishad1@ymail.com>
2019-09-03 12:22:19 +0600

    add privacy policy link

--- app/src/main/java/com/monir/expensetracker/ui/fragment/AboutFragment.java
+++ app/src/main/java/com/monir/expensetracker/ui/fragment/AboutFragment.java
@@ -4,9 +4,11 @@ package com.monir.expensetracker.ui.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.monir.expensetracker.R;
 
@@ -33,9 +35,9 @@ public class AboutFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_about, container, false);
 
+        TextView privacyPolicyTextView = view.findViewById(R.id.privacyPolicyTextView);
+        privacyPolicyTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
         return view;
-
-
     }
-
 }
